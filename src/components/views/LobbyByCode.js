030046801e152f5c700de5a9f0e0a608603585af
mkorob <maria.korobeynikova@uzh.ch>
2022-04-04 23:57:33 +0200

    closes #28, closes #36, closes #27, progress #39

---
+++ src/components/views/LobbyByCode.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import {api, handleError} from 'helpers/api';
+import {Button} from 'components/ui/Button';
+import 'styles/views/LobbyByCode.scss';
+import BaseContainer from "components/ui/BaseContainer";
+import { useHistory, useLocation, Link } from 'react-router-dom';
+import jsonDataLobbies from './jsonDataLobbies';
+
+
+// form of code
+const FormField = props => {
+  return (
+      <div>
+          <input
+              className="LobbyByCode input-name"
+              placeholder="enter code here..."
+              value={props.value}
+              onChange={e => props.onChange(e.target.value)}
+          />
+      </div>
+  );
+};
+
+const LobbyByCode = () => {
+  const history = useHistory();
+  const [codeInput, setCodeInput] = useState(null);
+  const lengthCode = 8;
+  //const [validCodes, setValidCodes] = useState(null);
+  const [lobbies, setLobbies] = useState(null);
+
+  // TODO - to change this to an API call  
+  //const ValidCodes = ["ABCDEFGH", "AAAAAAAA", "HANNIBAL"]
+  const ValidCodes = () => {
+    //const response = await api.get('/v1/game/lobbies');
+    //setLobbies(response.data);
+    const Codes = [];
+    jsonDataLobbies.map(lobby =>(
+      Codes.push(lobby.invitationCode)
+
+    ))
+
+    //setValidCodes(Codes);
+    return Codes
+  }
+
+  var validCodes = ValidCodes();
+
+  //TODO - find lobby of the ID
+  const findLobbyID = (un) => {
+    var lobbyNumber = validCodes.index(un);
+    return jsonDataLobbies[lobbyNumber].id;
+  }
+
+  const ValidateCode = (un) => {
+
+    setCodeInput(un);
+
+    // automatically enters the code if it is 8 digits
+    if(un.length === lengthCode) {
+       
+      if(validCodes.includes(un)) {
+        //alert("MATCHED");
+        // TODO - check if the lobby is still active? not complete? Will follow logic in server when completed
+        // TODO - update with the ID of the lobby that matches - through GET LOBBIES, all passwords and match id?
+        //var matchedID = findLobbyID(un);
+        //alert("CONNECTING TO LOBBY ID "+matchedID)
+        //var lobbyLink = `/lobby/${matchedID}`
+
+        // TODO - post player
+        //history.push(lobbyLink)
+        alert("MATCHED");
+        history.push('lobby/1');
+      }
+
+      else {
+        alert("Check Code!")
+      }
+    }
+
+  }
+
+
+
+  return (
+    <BaseContainer>
+    <div className="LobbyByCode container">
+      <h2 className=' LobbyByCode h2'> To join a private lobby, enter  the provided 8-digit code in the field below and press Join:</h2>
+      <FormField
+                                value={codeInput}
+                                onChange={un => ValidateCode(un)}>
+                            </FormField>
+       <Link className="LobbyByCode link"
+                    to={{
+                        pathname: '/join_QR'}}>
+                Join using a QR code instead</Link>
+          <div className="LobbyByCode button-container">
+            <Button
+              width="100%"
+              onClick={() =>  history.push('/public-lobbies')}
+            >
+              RETURN TO PUBLIC LOBBIES
+            </Button>
+            </div>
+            <div className="LobbyByCode button-container">
+            <Button className = "secondary-button return"
+              width="100%"
+              onClick={() => history.push('/home-page')}
+            >
+              RETURN HOME
+            </Button>
+          </div>
+          </div>
+    </BaseContainer>
+  );
+};
+
+export default LobbyByCode;
--- src/components/views/PublicLobbies.js
+++ src/components/views/PublicLobbies.js
@@ -6,6 +6,8 @@ import 'styles/views/HomePage.scss';
 import 'styles/views/PublicLobbies.scss';
 import BaseContainer from "components/ui/BaseContainer";
 import jsonDataLobbies from "./jsonDataLobbies";
+import {useEffect} from "react";
+import Header from './Header';
 
 /*
 It is possible to add multiple components inside a single file,
@@ -14,16 +16,75 @@ As a rule of thumb, use one file per component and only add small,
 specific components that belong to the main one in the same file.
  */
 
+
 const PublicLobbies = () => {
   const history = useHistory();
+  //const RESTDataLobbies = api.get(URL = '/v1/game/lobbies/');
+  const [lobbyData, setLobbyData] = useState(null);
 
   const returnHome = () => {
     history.push('/home-page');
   }
 
+
   // TODO - UPDATE WITH JOINBYCODE PAGE
   const joinLobbybyCode = () => {
-    history.push('/home-page');
+    history.push('/join-code');
+  }
+
+  // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
+  useEffect(() => {
+    // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+    async function fetchData() {
+      try {
+        const response = await api.get('/v1/game/lobby/1');
+
+        // delays continuous execution of an async operation for 1 second.
+        // This is just a fake async call, so that the spinner can be displayed
+        // feel free to remove it :)
+
+        // Get the returned users and update the state.
+        //setLobbyData(response.data);
+
+        // This is just some data for you to see what is available.
+        // Feel free to remove it.
+        console.log('request to:', response.request.responseURL);
+        console.log('status code:', response.status);
+        console.log('status text:', response.statusText);
+        console.log('requested data:', response.data);
+
+        // See here to get more data.
+        console.log(response);
+      } catch (error) {
+        console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+        console.error("Details:", error);
+        //alert("Something went wrong while fetching the users! See the console for details.");
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  let content = null;
+
+  if (jsonDataLobbies) {
+    content = (
+      <div>
+          {jsonDataLobbies.map((data, key) => (
+          <div key={key}>
+          <LobbyInfo
+            id = {data.id}
+            key={key}
+            name={data.name}
+            mode={data.mode}
+            players={data.players}
+            visibility = {data.visibility}
+          />
+        </div>
+          ))}
+        
+      </div>
+    );
   }
 
   // TODO - jsonDataLobbies import from REST or whatever
@@ -42,19 +103,7 @@ const PublicLobbies = () => {
 </tr>
 </table>
 <table className= "PublicLobbies table">
-        {jsonDataLobbies.map((data, key) => {
-          return (
-            <div key={key}>
-              <LobbyInfo
-                key={key}
-                name={data.name}
-                mode={data.mode}
-                players={data.players}
-                capacity={data.capacity}
-              />
-            </div>
-          );
-        })}
+        {content}
         </table>
         <div className="PublicLobbies button-container">
             <Button
@@ -79,28 +128,29 @@ const PublicLobbies = () => {
   );
       };
 
-  const LobbyInfo = ({ name, mode, players, capacity }) => {
+  const LobbyInfo = ({id, name, mode, players, visibility}) => {
 
   const history = useHistory();
+  const displayedMode = mode === "ONE_VS_ONE" ? "1v1" : "2v2";
+  const presentPlayers = players.length;
+  const totalPlayers = mode === "ONE_VS_ONE" ? 2 : 4;
 
-  // TODO - update with to lobby/id instead
-  const gotoLobbyPage = () => {
-    history.push('/home-page');
-  }
-        if (!name) return <div />;
+  var userlink = `/lobby/${id}`
+
+        if (visibility == "PRIVATE") return <div />;
         return (
-              <tr onClick={() => gotoLobbyPage()}>
+              <tr onClick={() =>  history.push(userlink)}>
                 <td>
                   {name}
                 </td>
                 <td>
-                  {mode}
+                  {displayedMode}
                 </td>
                 <td>
-                  {players}
+                  {presentPlayers}
                 </td>
                 <td>
-                  {capacity}
+                  {totalPlayers}
                 </td>
               </tr>
         );
