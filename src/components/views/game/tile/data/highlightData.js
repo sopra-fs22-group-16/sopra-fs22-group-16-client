4ea48fb928591daee51a55b96d5cf7dc4fca28df
Alessio Brazerol <alessio.bra@hotmail.ch>
2022-04-13 17:44:35 +0200

    Revert "Revert "Revert "Revert "MapView improvements. Restructured map, to use TileModel and split tile into its parts. Added support for animations""""
    This reverts commit 6c26a5a54aaccc777f0699e8c4d884d21626b32a.

---
+++ src/components/views/game/tile/data/highlightData.js
@@ -0,0 +1,11 @@
+export const highlightData = {
+    hostileUnit: {
+        path:"/tiles/tileHighlight/hostile_unit_indicator.png",
+    },
+    attackRange: {
+        path:"/tiles/tileHighlight/attack_range_indicator.png",
+    },
+    movementRange: {
+        path:"/tiles/tileHighlight/movement_range_indicator.png",
+    }
+}
