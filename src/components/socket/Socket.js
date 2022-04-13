38bdcc96703615bfc10bb0a6dd048f620d502d4f
luis-tm <luis.torrejonmachado@uzh.ch>
2022-04-13 12:06:10 +0200

    socket connection; client part, code comment - #140

--- src/components/socket/Socket.js
+++ src/components/socket/Socket.js
@@ -3,6 +3,8 @@ import {getDomain} from 'helpers/getDomain';
 
 const Socket = props => {
 
+    // when receiving a message, Socket component will refresh the view 
+    // which uses it.
     const onMessageReceive = () => {
         window.location = window.location.href;
     }
